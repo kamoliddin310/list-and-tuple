1ba07b030823e25ae07936f75fda3ff904638bca
kamoliddin310 <kamoliddin310pubg@gmail.com>
2025-01-17 04:48:44 +0500

    4 qoldi

---
+++ Tuple.Methods/5.py
@@ -0,0 +1,11 @@
+a = (1, 2, 3, 2, 4, 3, 2, 5, 6, 1, 1)
+
+max = None
+count = 0
+
+for i in set(a):
+    count = a.count(i)
+    if count > count:
+        count = count
+        max = i
+print(max)
