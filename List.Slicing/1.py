cff2420d70a2d46ef0748bff47a6c0e8098039b0
kamoliddin310 <kamoliddin310pubg@gmail.com>
2025-01-17 03:55:39 +0500

    bunda ham 3-misol muommosi

---
+++ List.Slicing/1.py
@@ -0,0 +1,2 @@
+a = ["salom", "nima", "qonday", 123, "qayerlarda", 76.3, "salomatlar", 32]
+print(a[2:7])
---
+++ List.Slicing/2.py
@@ -0,0 +1,4 @@
+a = ["salom", "nima", "qonday", 123, "qayerlarda", 76.3, "salomatlar", 32]
+
+for i in range(2, len(a), 3):
+    print(a[i])
