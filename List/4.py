ca7fefa745eda3bc81aa2944c934e7ea0103c364
kamoliddin310 <kamoliddin310pubg@gmail.com>
2025-01-17 03:20:24 +0500

    3-misol haqida tuliq ma'lumot yuq ekan

---
+++ List/4.py
@@ -0,0 +1,4 @@
+a = [12, 43, 123, 32, 21, 97, 19, 31, 27, 98, 100]
+for i, v in enumerate(a):
+    if v % 2 == 0:
+        print(i,"-indexda  --> ", v)
---
+++ List/5.py
@@ -0,0 +1,5 @@
+a = ["SALOM", "NIMA gaplar", "ZURU", "qachon ENDI"]
+print("asl kurinishi --> ", a)
+
+for i, v in enumerate(a):
+    print(i,"-indexda --> ", v.lower())
